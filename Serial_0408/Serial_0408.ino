0255ef4fa96dbbdba41f6c30d13a6df892da6b7d
Dahyeon Han <52192706+ekgus9701@users.noreply.github.com>
2021-04-08 13:55:10 +0900

    0408 class

---
+++ Serial_0408/Serial_0408.ino
@@ -0,0 +1,40 @@
+/*int sum=0;
+int loop_count=0;
+*/
+int button_state;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(3,INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ /*Serial.print("내가 하고싶은 말은...");
+ Serial.print("    ");
+*/
+
+/*
+ loop_count=loop_count+1;
+ sum=sum+loop_count;
+ Serial.print(loop_count);
+ Serial.print(", ");
+ Serial.println(sum);
+ delay(1000);
+ */
+/*
+Serial.println(68,BIN);
+delay(1000);
+ */
+
+/*Serial.write(68); 
+delay(1000);
+*/
+
+/*
+button_state=digitalRead(3);
+Serial.println(button_state);
+delay(100);
+*/
+}
